b475a5efa04bdc587067d76f215478e2b89ab008
Alex Wallace <alexwallax@gmail.com>
2021-11-29 16:03:08 -0300

    commit 02

--- index.php
+++ index.php
@@ -3,3 +3,26 @@
 
     //instanciar a class usuarios
     $u = new Usuarios();
+
+
+    //selecionar
+    /*
+    $res = $u->selecionar(1);
+    print_r($res);
+    */
+
+
+    //inserir
+    /*
+    $u->inserir("Jose", "jose@gmail.com", "123");
+    */
+
+    //atualizar
+    /*
+    $u->atualizar("Maria", "maria@ig.com", "222", 3);
+    */
+
+    //excluir
+    /*
+    $u->excluir(3);
+    */
